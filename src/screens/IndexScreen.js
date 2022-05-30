f416887e4de708afdb21a9a9fe1ca1c94031e166
Gabe Cloud <gabecloud94@gmail.com>
2022-05-30 17:40:32 -0600

    initial commit

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View, FlatList, Button } from "react-native";
+import React, { useContext } from "react";
+import { Context } from "../context/BlogContext";
+
+const IndexScreen = () => {
+  const { state, addBlogPost } = useContext(Context);
+  return (
+    <View>
+      <Text>IndexScreen</Text>
+      <Button title="Add Post" onPress={addBlogPost} />
+      <FlatList
+        data={state}
+        keyExtractor={(blogPost) => blogPost.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>;
+        }}
+      />
+    </View>
+  );
+};
+
+export default IndexScreen;
+
+const styles = StyleSheet.create({});
